898791f39cc6346773d82d1a0417fcde55f7952b
Ekaterina <yeldog@mail.ru>
2021-07-28 22:32:00 +0300

    baseCreated

--- app/src/main/java/com/example/dibujosdeldia/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/dibujosdeldia/ui/main/MainFragment.kt
@@ -1,33 +1,69 @@
 package com.example.dibujosdeldia.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import coil.api.load
 import com.example.dibujosdeldia.R
 import com.example.dibujosdeldia.databinding.MainFragmentBinding
+import com.example.dibujosdeldia.ui.main.picture.PictureOfTheDayData
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.getData().observe(viewLifecycleOwner,
+            Observer<PictureOfTheDayData> { renderData(it) })
+    }
+
+    private fun renderData(data: PictureOfTheDayData) = with(binding) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    Toast.makeText(context, getString(R.string.link_is_empty), Toast.LENGTH_SHORT).show()
+                } else {
+                    //showSuccess()
+                    image_view.load(url) {
+                        lifecycle(this@MainFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                        waitForIt.visibility = View.GONE
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                waitForIt.visibility = View.VISIBLE
+            }
+            is PictureOfTheDayData.Error -> {
+                waitForIt.visibility = View.GONE
+                Toast.makeText(context, getString(R.string.error), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     companion object {
         fun newInstance() = MainFragment()
     }
-
 }
