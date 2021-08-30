0d48a3656c132aea2867939bfd3380437a85b858
Ekaterina <yeldog@mail.ru>
2021-08-30 14:12:22 +0300

    splash aded

--- app/src/main/java/com/example/dibujosdeldia/ui/main/api/CuriosityInfoFragment.kt
+++ app/src/main/java/com/example/dibujosdeldia/ui/main/api/CuriosityInfoFragment.kt
@@ -6,8 +6,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.Spannable
 import android.text.style.BulletSpan
-import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,29 +42,37 @@ class CuriosityInfoFragment : Fragment() {
 
     private fun textDecor() {
         //FHAZ
-        spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(
-            requireContext(), R.color.purple_500)), 539, 540, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 539, 543, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        val typedValue = TypedValue()
+        context?.theme?.resolveAttribute(R.attr.fab_back, typedValue, true)
+        spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(requireContext(),
+            typedValue.resourceId)), 539, 540, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 539, 543,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         //RHAZ
         spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(
             requireContext(), R.color.purple_500)), 578, 579, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 578, 582, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 578, 582,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         //MAST
         spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(
             requireContext(), R.color.purple_500)), 615, 616, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 615, 619, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 615, 619,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         //CHEMCAM
         spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(
             requireContext(), R.color.purple_500)), 664, 671, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 664, 672, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 664, 672,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         //NAVCAM
         spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(
             requireContext(), R.color.purple_500)), 730, 735, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 730, 736, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 730, 736,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         //MAHLI
         spannableMarsDescription.setSpan(BulletSpan(8, ContextCompat.getColor(
             requireContext(), R.color.purple_500)), 765, 766, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 765, 770, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 765, 770,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         //marcers
         spannableMarsDescription.setSpan(StyleSpan(Typeface.BOLD), 9, 21,
